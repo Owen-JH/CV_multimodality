45a3c9b3479c17ddc2e0d2338160e2e351c42f6b
Sunburst <142214047+Owen-JH@users.noreply.github.com>
2026-02-23 09:12:28 +0800

    Refactor PCA script to remove hardcoded paths

--- PCA.py
+++ PCA.py
@@ -4,7 +4,7 @@ from sklearn.decomposition import PCA
 import numpy as np
 
 # 读取Excel文件
-file_path = r'C:\大学\大四\大四下\毕设\数据集\data1.xlsx'
+file_path = 'data1.xlsx'
 data= pd.read_excel(file_path)
 scaler = StandardScaler()
 # Step 1: Drop 'US_Number' and 'Diagnosis' columns
@@ -30,12 +30,12 @@ loading_df = pd.DataFrame(loadings.T,
                           index=df_cleaned.columns)
 
 # 导出前26个主成分的负载矩阵
-loading_df.to_excel(r'C:\大学\大四\大四下\毕设\数据集\PCA_Loadings.xlsx', index=True)
+loading_df.to_excel('PCA_Loadings.xlsx', index=True)
 
 # 将前26个主成分数据导出
 pca_26_components = pca_components[:, :26]
 pca_26_df = pd.DataFrame(pca_26_components, columns=[f'PC{i+1}' for i in range(26)])
-pca_26_df.to_excel(r'C:\大学\大四\大四下\毕设\数据集\PCA_Components.xlsx', index=False)
+pca_26_df.to_excel('PCA_Components.xlsx', index=False)
 
 # 统计每个主成分中绝对值最大的前10个特征
 top_features = {}
@@ -49,4 +49,5 @@ for pc in loading_df.columns:
 # 转换为DataFrame并排序
 top_features_df = pd.DataFrame(list(top_features.items()), columns=['Feature', 'Count'])
 top_features_df = top_features_df.sort_values(by='Count', ascending=False).reset_index(drop=True)
+
 print(top_features_df)
